3e8ed871f190a19f54afaaa0b292562864db138e
Phe0 <pheofo@gmail.com>
2018-09-16 15:21:07 -0300

    Small changes do Presentation css

--- curriculum/src/Components/Presentation.js
+++ curriculum/src/Components/Presentation.js
@@ -8,8 +8,9 @@ class Presentation extends Component {
       <div className="Presentation">
         <img className="foto" src={require('../img/Pedro.jpg')} alt="Foto Pedro" />
         <h2 className="nome" >Pedro Henrique Andrade Féo</h2>
-        <p className="resumo" >Estudante de Engenharia de Software</p>
-        <p className="resumo" >Brasileiro, Solteiro, 19 Anos</p>
+        <p className="resumo" >Estudante de Engenharia de Software<br></br>
+        Brasileiro, Solteiro, 19 Anos
+        </p>
         <img className="seta" src={require('../img/seta.svg')} alt="Seta"/>
       </div>
     );
