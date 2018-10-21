d46da1f4cc932f1d8fb161a0305447beface90af
Pedro Féo <phe0@pop-os.localdomain>
2018-10-20 21:29:39 -0300

    Add Soft Skills

--- src/Components/HabilidadesInteresses/HabilidadesInteresses.js
+++ src/Components/HabilidadesInteresses/HabilidadesInteresses.js
@@ -3,11 +3,13 @@ import ButtonLike from './ButtonLike'
 import interesses from '../../json/Interesses.json'
 import habilidades from '../../json/Habilidades.json'
 import '../../stylesheet/HabilidadesInteresses.css';
+import softskills from '../../json/SoftSkills.json'
 
 const HabilidadesInteresses = () => {
   return (
     <div className="HabilidadesInteresses" id="habilidades-interesses">
-      <ButtonLike id={'Habilidades'} info={habilidades}/>
+      <ButtonLike id={'Hard Skills'} info={habilidades}/>
+      <ButtonLike id={'Soft Skills'} info={softskills}/>
       <ButtonLike id={'Interesses'} info={interesses}/>
     </div>
   );
