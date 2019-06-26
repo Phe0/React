ec77a2015f90b2a771f5b56fae129b56c3f9b00a
phe0 <pheofo@gmail.com>
2019-06-26 01:22:45 -0300

    Update

--- src/Components/Contato/Contato.js
+++ src/Components/Contato/Contato.js
@@ -9,8 +9,6 @@ const Contato = () => {
     return(
         <div className="contato-area">
             <h2 className="title" >Entre em contato</h2>
-            <LinhaContato image={whatsapp} text={'+55(61)98294-7102'} />
-            <LinhaContato image={telegram} text={'@Feozineo'} />
             <LinhaContato image={carta} text={'pheofo@gmail.com'} />
             <div className="github-linkedin">
                 <a href={`https://github.com/Phe0`} >
