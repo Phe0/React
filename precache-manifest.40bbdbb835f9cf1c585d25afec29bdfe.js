305f14bd0416bf977a7d5099678ead327f86cc72
phe0 <pheofo@gmail.com>
2019-06-28 21:20:36 -0300

    Updates

---
+++ precache-manifest.40bbdbb835f9cf1c585d25afec29bdfe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e27e6365b5068d47616d02b1643f33",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "83a008de5ab63fe17dc0",
+    "url": "/React/static/css/main.b2150480.chunk.css"
+  },
+  {
+    "revision": "72bab101a2c291f381f6",
+    "url": "/React/static/js/2.36bfa19e.chunk.js"
+  },
+  {
+    "revision": "83a008de5ab63fe17dc0",
+    "url": "/React/static/js/main.727f36b5.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "d9f03fbbe1017ced49070baa9be29159",
+    "url": "/React/static/media/C++-white.d9f03fbb.png"
+  },
+  {
+    "revision": "2c1b9de75efdbff5ae8eb8d79de45acf",
+    "url": "/React/static/media/Javascript-white.2c1b9de7.png"
+  },
+  {
+    "revision": "7ebafea04646d0f0ca5958527c4cc859",
+    "url": "/React/static/media/MePink.7ebafea0.svg"
+  },
+  {
+    "revision": "7ada5d8d10a447dd56df86ea2e1c7c08",
+    "url": "/React/static/media/MePurple.7ada5d8d.svg"
+  },
+  {
+    "revision": "730200bbf4011ec1ff5290acac198e75",
+    "url": "/React/static/media/MePurpleish.730200bb.svg"
+  },
+  {
+    "revision": "ad148cbb786bb8a95aa8c682d29e68cf",
+    "url": "/React/static/media/Python-white.ad148cbb.png"
+  },
+  {
+    "revision": "8b02a882042585f358b6623a861ae601",
+    "url": "/React/static/media/ReactJS-white.8b02a882.png"
+  },
+  {
+    "revision": "2b9e76a5674df54c7c628bc8e76a1f92",
+    "url": "/React/static/media/Ruby on Rails-white.2b9e76a5.png"
+  },
+  {
+    "revision": "516a67d197cc06d03e32996f5ef98a8c",
+    "url": "/React/static/media/linkedin-white.516a67d1.png"
+  },
+  {
+    "revision": "43e56f44b346ed69b62dd9ec2aaa0dfd",
+    "url": "/React/static/media/php-white.43e56f44.png"
+  },
+  {
+    "revision": "27bb0c8f1c7ccde8ad59eddaf2170a5b",
+    "url": "/React/static/media/seta2.27bb0c8f.svg"
+  },
+  {
+    "revision": "426d7c1e4fcc3ddb5bf0a0f445ac7e5e",
+    "url": "/React/static/media/telegram-white.426d7c1e.png"
+  },
+  {
+    "revision": "cd445a07e3139f50fb6f8ac88e8a96e1",
+    "url": "/React/static/media/whatsapp-white.cd445a07.png"
+  }
+]);
